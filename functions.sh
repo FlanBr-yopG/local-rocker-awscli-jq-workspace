3226f6ce2bd23631f9bbeb46fb534150b4832b6c
Brian Flannery <brian.flannery@pearson.com>
2017-11-14 16:06:08 -0700

    ansible, python, terraform-inventory

--- functions.sh
+++ functions.sh
@@ -1,11 +1,13 @@
 
 # # CONSTS
 
-use_docker_for_binaries="gcloud jq packer terraform"
+# # NOTE: List of binaries provided through Docker containers instead of local install:
+use_docker_for_binaries="gcloud jq packer terraform terraforminventory"
 # # Why not garland/aws-cli-docker ?
 # #  Well, I prefer "aws --version" 
 # #   to say      "aws-cli/1.11.170 Python/2.7.10 Darwin/16.7.0 botocore/1.7.28"
-# #   rather than "aws-cli/1.11.131 Python/2.7.13 Linux/4.9.49-moby botocore/1.5.94".
+# #   rather than "aws-cli/1.11.131 Python/2.7.13 Linux/4.9.49-moby botocore/1.5.94"
+# #   ... on a Darwin/MacOS box.
 # #  But it also seems healthier to trust our the Homebrew formulas list more than some unofficial Docker Hub image.
 
 # # DEFAULTS
@@ -22,6 +24,8 @@ _main() {
   ensure_virtualbox
   ensure_vagrant
   ensure_kubectl
+  ensure_python3
+  ensure_ansible
 }
 
 _ensure_it() {
@@ -75,6 +79,9 @@ die() {
   echo $1;
   CHECKS_RETURNS=1
   exit 1;
+}
+ensure_ansible() {
+  _ensure_it ansible '' true
 }
 ensure_awscli() {
   _ensure_it 'AWS CLI' aws
@@ -146,6 +153,9 @@ ensure_kubectl() {
 ensure_make() {
   _ensure_it "XCode (for make, etc.)" make
 }
+ensure_python3() {
+  _ensure_it python3 '' true
+}
 ensure_rocker() {
   _ensure_it Rocker rocker
 }
@@ -181,6 +191,12 @@ get_admin_gid() {
   fi ;
   echo $gid
 }
+install_ansible() {
+  # Alternate = https://hub.docker.com/r/williamyeh/ansible/
+  # Alternate = brew install ansible
+  ensure_python3
+  sudo bash -c "umask 0022; pip3 install ansible"
+}
 install_aws() {
   brew install awscli
 }
@@ -196,6 +212,9 @@ install_kubectl() {
 install_make() {
   xcode-select --install
 }
+install_python3() {
+  brew install python3
+}
 install_rocker() {
   local result=0
   brew tap grammarly/tap \
@@ -210,18 +229,23 @@ mk_bash_every_time() {
 
 main() {
   path_add ~/bin
+  path_unshift ~/bin/precedence
 }
 
 path_add() { # cf. RHEL's pathmunge command
   local path_to_add=$1
   [[ ":$PATH:" != *":$path_to_add:"* ]] && export PATH="$PATH:$path_to_add"
 }
+path_unshift() {
+  local path_to_add=$1
+  [[ ":$PATH:" != *":$path_to_add:"* ]] && export PATH="$path_to_add:$PATH"
+}
 
 main
 EOFbetg
 }
 mk_bin_gcloud() {
-  cat > ~/bin/gcloud <<'EOFbg'
+  cat > ~/bin/gcloud <<'EOFgcld'
 #!/bin/bash
 
 image='google/cloud-sdk:alpine'
@@ -241,10 +265,10 @@ main() {
 }
 
 main "$@"
-EOFbg
+EOFgcld
 }
 mk_bin_jq() {
-  cat > ~/bin/jq <<'EOFbg'
+  cat > ~/bin/jq <<'EOFjq'
 #!/bin/bash
 
 image='stedolan/jq'
@@ -257,12 +281,12 @@ main() {
 }
 
 main "$@"
-EOFbg
+EOFjq
 }
 mk_bin_kubectl() {
   # # NOTE: Running kubectl as a Docker-contained binary is dumb. Just run it locally.
   # Alternate = brew install kubectl
-  cat > ~/bin/kubectl <<'EOFbg'
+  cat > ~/bin/kubectl <<'EOFkc'
 #!/bin/bash
 
 image='lachlanevenson/k8s-kubectl:latest'
@@ -276,10 +300,10 @@ main() {
 }
 
 main "$@"
-EOFbg
+EOFkc
 }
 mk_bin_packer() {
-  cat > ~/bin/packer <<'EOFbg'
+  cat > ~/bin/packer <<'EOFp'
 #!/bin/bash
 
 image='hashicorp/packer:light'
@@ -292,25 +316,122 @@ main() {
 }
 
 main "$@"
-EOFbg
+EOFp
 }
 mk_bin_terraform() {
   # Alternate = brew install terraform
-  cat > ~/bin/terraform <<'EOFbg'
+  cat > ~/bin/terraform <<'EOFtfs'
 #!/bin/bash
 
-image='hashicorp/terraform:light'
+# image='hashicorp/terraform:light'
+# image='hashicorp/terraform:0.8.8'
+# image='hashicorp/terraform:0.7.0'
+# image='hashicorp/terraform:0.7.13'
+image='hashicorp/terraform:0.7.6'
 
 main() {
     docker run --rm -it \
-      -v $(pwd):/app/ -w /app/ \
+      -v $(get_host_volume):/app/ -w /$(get_container_volume)/ \
       "$image" \
       "$@"
     #
 }
 
+die() {
+  echo $1
+  exit 1
+}
+get_host_volume() {
+  local pwd0=$(pwd)
+  (cd ../../.. && pwd) || die "Failed to cd ../.. from $pwd0: $?."
+}
+get_container_volume() {
+  local pwd0=$(pwd)
+  local bn1=$(basename $pwd0)
+  local dn1=$(dirname $pwd0)
+  local bn2=$(basename $dn1)
+  local dn2=$(dirname $dn1)
+  local bn3=$(basename $dn2)
+  echo "app/$bn3/$bn2/$bn1"
+}
+
 main "$@"
-EOFbg
+EOFtfs
+}
+mk_bin_terraforminventory() {
+  cat > ~/bin/terraforminventory <<'EOFtfwi'
+#!/bin/bash
+
+container=terraform-inventory
+image=yopgflanbr-terraform-inventory
+version=0.0.5
+tfi_v=v0.7-pre
+
+main() {
+  cat > Dockerfile <<'EOFdf'
+FROM alpine:latest
+
+RUN apk add --update git curl openssh
+RUN apk add --no-cache unzip
+ARG APP_VERSION
+RUN curl -sL https://github.com/adammck/terraform-inventory/releases/download/${APP_VERSION}/terraform-inventory_${APP_VERSION}_linux_amd64.zip > tfi.zip
+RUN unzip tfi.zip -d /bin || od -cx tfi.zip && \
+    rm -f tfi.zip
+
+RUN ls -lart /bin/
+
+ENTRYPOINT ["/bin/terraform-inventory"]
+EOFdf
+  # set -exu
+  local already_image=$(docker images -f=reference="$image:$version" --format '{{.ID}}')
+  [[ $already_image ]] && echo "NOTE: Image '$image:$version' already exists; not re-building." 1>&2 \
+  || {
+    rm -rf tmp &>/dev/null || true ;
+    mkdir tmp ; cp Dockerfile tmp/ ;
+    local pwd0=$(pwd)
+    cd tmp ;
+    docker build --rm -t "$image:$version" \
+      --build-arg APP_VERSION=$tfi_v \
+      . \
+    ;
+    cd "$pwd0"
+  }
+  local already_running=$(docker ps -f "name=$container" --format '{{.ID}}')
+  [[ $already_running ]] && {
+    if running_this_version; then
+      echo "WARNING: Container '$container' is already running."; 1>&2 ;
+    else
+      echo "NOTE: Already running but wrong version. Stopping and re-running." 1>&2 ;
+      docker rm -f $container
+      already_running=''
+    fi ;
+  }
+  local container_exists=$(docker ps -a -f "name=$container" --format '{{.ID}}')
+  [[ $already_running ]] || {
+    if [[ $container_exists ]] ; then
+      echo "WARNING: Container '$container' already exists. Removing it now." 1>&2 ;
+      docker rm -f $container
+    fi ;
+    echo "NOTE: Not already running. Starting it now." 1>&2 ;
+    docker run --rm -it \
+      --name $container \
+      -v $(pwd):/app/ -w /app/ \
+      "$image:$version" \
+      "$@"
+    #
+  }
+}
+running_this_version() {
+  local what_runs=$(docker ps -a -f "name=$container" --format '{{.Image}}')
+  [[ $what_runs == "$image:$version" ]] || {
+    echo "NOTE: Already running '$container' container is the wrong version. Updating it from $what_runs to version $version." 1>&2 ;
+    return 1
+  }
+}
+
+main "$@"
+EOFtfwi
+  ln -s terraforminventory ~/bin/terraform-inventory
 }
 must_be_admin_error() {
   local what=$1
